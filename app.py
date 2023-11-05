e98970a3be2caad0a89cf1c1a581d8f7472b604e
Alexander Krivikov <adk26@tpu.ru>
2023-11-05 19:33:42 +0700

    init: init project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from pages.main.main import main
+from pages.about.about import about
+from pages.settings.settings import settings
+
+app = Flask(__name__, template_folder="partials")
+app.register_blueprint(main, url_prefix="/")
+app.register_blueprint(about, url_prefix="/about")
+app.register_blueprint(settings, url_prefix="/settings")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
