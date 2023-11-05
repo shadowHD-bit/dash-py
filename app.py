3bb99e39fa75381711c078f590f0f0559fad9212
Alexander Krivikov <adk26@tpu.ru>
2023-11-06 03:43:40 +0700

    feat: try to add dash graph

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template
 from pages.main.main import main
 from pages.about.about import about
 from pages.settings.settings import settings
+from pages.dataset.dataset import dataset
 
 app = Flask(__name__, template_folder="partials", static_folder="statics")
+
 app.register_blueprint(main, url_prefix="/")
 app.register_blueprint(about, url_prefix="/about")
 app.register_blueprint(settings, url_prefix="/settings")
+app.register_blueprint(dataset, url_prefix="/dataset")
 
 @app.route('/')
 def index():
