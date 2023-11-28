45b3908cecba7b9c750c304c7131be557d8e7391
Alexander Krivikov <adk26@tpu.ru>
2023-11-28 21:49:26 +0700

    feat: ended refunds page

--- pages/refunds/refunds.py
+++ pages/refunds/refunds.py
@@ -1,11 +1,12 @@
 from datetime import date
 import dash
-from dash import html, dcc, callback, Output, Input
+from dash import html, dcc, callback, Output, Input, dash_table
 import dash_bootstrap_components as dbc
 from data import MAIN_DF, RETURNS_DF
 import pandas as pd
 from partials.statistic_card import build_statistic_card
 import plotly.express as px
+import dash_ag_grid as dag
 
 dash.register_page(__name__, name="Возвраты", path='/refunds', order=3)
 
@@ -15,7 +16,6 @@ merged_df = pd.merge(return_df, main_df, on='Order ID')
 
 list_region = RETURNS_DF['Region'].value_counts()
 list_prod = merged_df['Product Name'].value_counts()
-
 
 layout = html.Div([
     dbc.Row(className="mt-2 mb-3", children=[
@@ -99,7 +99,94 @@ layout = html.Div([
             ], start_collapsed=True),
         ], xs=12),
     ]),
+    dbc.Row(className="mt-2 mb-3", children=[
+        html.P('Список возвратов', className='title_content__block')
+    ]),
+    dbc.Row(className='mb-3', children=[
+        dbc.Col(children=[
+            dcc.DatePickerRange(
+                id='picker_date_ref',
+                min_date_allowed=date(2012, 1, 1),
+                max_date_allowed=date(2015, 12, 30),
+                start_date=date(2012, 1, 1),
+                end_date=date(2015, 12, 30),
+                display_format='D/M/Y'
+            ),
+        ], xs=3),
+        dbc.Col(children=[
+            dcc.Dropdown(
+                ['Technology', 'Furniture', 'Office Supplies'],
+                id="picker_cat_ref",
+                placeholder="Выберите категорию",
+                value="Technology"
+            )
+        ], xs=3),
+        dbc.Col(id='container_dropdown_sub', children=[
+        ], xs=3),
+    ]),
+    dbc.Row(id='list_ref', children=[
+
+    ]),
 ])
+
+
+@callback(
+    Output("container_dropdown_sub", "children"),
+    Input("picker_cat_ref", "value"),
+)
+def display_subcategory_dropown_call(n):
+    sub_df = merged_df.loc[merged_df['Category'] == n]
+    sub_list = sub_df['Sub-Category'].unique()
+    dropdown = dcc.Dropdown(
+        sub_list,
+        id="picker_sub_ref",
+        placeholder="Выберите подкатегорию"
+    ),
+    return dropdown
+
+
+@callback(
+    Output("list_ref", "children"),
+    [Input("picker_sub_ref", "value"),
+     Input('picker_date_ref', 'start_date'),
+     Input('picker_date_ref', 'end_date')]
+)
+def display_subcategory_dropown_callback(n, start_date, end_date):
+    if n:
+        sub_df = merged_df.loc[merged_df['Sub-Category'] == n]
+        list_df = sub_df[(merged_df['Order Date'] > str(start_date)) & (
+            merged_df['Order Date'] < str(end_date))]
+        df_ref = list_df.loc[:, ['Order ID', 'Order Date',
+                                 'Product ID', 'Product Name', 'Region_x']]
+
+        # columnDefs = [
+        #     {"field": "Order ID"},
+        #     {"field": "Order Date"},
+        #     {"field": "Product ID"},
+        #     {"field": "Product Name"},
+        #     {"field": "Region_x"},
+        # ]
+
+        # list_ = dag.AgGrid(
+        #     id="enable-pagination",
+        #     columnDefs=columnDefs,
+        #     rowData=list_df,
+        #     columnSize="sizeToFit",
+        #     defaultColDef={"resizable": True, "sortable": True, "filter": True},
+        #     dashGridOptions={"pagination": True},
+        # ),
+
+        list_ = dash_table.DataTable(df_ref.to_dict('records'),
+                                     [{"name": i, "id": i}
+                                         for i in df_ref.columns],
+                                     page_size=30,
+                                     style_table={
+            'overflowX': 'scroll'
+        })
+
+        return list_
+    else:
+        return ""
 
 
 @callback(
@@ -142,8 +229,8 @@ def display_subcategory_dropown_callback(n):
 @callback(
     Output("prod_graph__container", "children"),
     [Input("sub_product_dropdown", "value"),
-    Input('picker_date_prod', 'start_date'),
-    Input('picker_date_prod', 'end_date')]
+     Input('picker_date_prod', 'start_date'),
+     Input('picker_date_prod', 'end_date')]
 )
 def display_subcategory_dropown_callback(n, start_date, end_date):
     list_df = merged_df[(merged_df['Order Date'] > str(start_date)) & (
@@ -153,7 +240,8 @@ def display_subcategory_dropown_callback(n, start_date, end_date):
     if n and len(list_dfs):
         lists = list_dfs['Product Name'].value_counts()
         fig = px.bar(lists, x=lists.index, y=lists.values)
-        fig.update_layout(xaxis_title="Product Name", yaxis_title="Count Refunds")
+        fig.update_layout(xaxis_title="Product Name",
+                          yaxis_title="Count Refunds")
         fig
 
         children = html.Div(children=[
