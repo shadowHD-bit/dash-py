c49bfce2cf65e186a5f048057d96f86b9084120d
Alexander Krivikov <adk26@tpu.ru>
2023-11-28 12:18:24 +0700

    feat: change dataset pages

--- data.py
+++ data.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
+# Get orders dataframe from XLSX table
 MAIN_DF = pd.read_excel('data\data.xlsx')
+
+# Get returns dataframe from XLSX table
+RETURNS_DF = pd.read_excel('data\data.xlsx', sheet_name='Returns')
--- pages/dataset/dataset.py
+++ pages/dataset/dataset.py
@@ -3,10 +3,32 @@ from dash import html, dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
 
-dash.register_page(__name__, title="Информационная панель | Датасет", name="Датасет", path='/dataset', order=1)
+from data import MAIN_DF, RETURNS_DF
 
-df = pd.read_excel('data\data.xlsx')
+dash.register_page(__name__, title="Информационная панель | Датасет",
+                   name="Датасет", path='/dataset', order=1)
 
+df = MAIN_DF
+returns_df = RETURNS_DF
+
+tab1_content = dbc.Row([
+    dash_table.DataTable(df[:300].to_dict('records'),
+                         [{"name": i, "id": i} for i in df.columns],
+                         page_size=30,
+                         style_table={
+        'overflowX': 'scroll'
+    }
+    )
+])
+
+tab2_content = dbc.Row([
+    dash_table.DataTable(returns_df[:300].to_dict('records'),
+                         [{"name": i, "id": i} for i in returns_df.columns],
+                         page_size=30,
+                         style_table={
+        'overflowX': 'scroll'
+    })
+])
 layout = html.Div([
     dbc.Row(style={'margin': '10px'}, children=[
         dbc.Row([
@@ -16,17 +38,12 @@ layout = html.Div([
             dbc.Alert("Из-за большого размера исходного датасета, на странице выводяться только первые 300 строк исходного файла XLSX. Чтобы ознакомиться с полным датасетом, нажмите кнопку 'Скачать'.", color="info"),
         ]),
         dbc.Row([
-            dbc.Button("Скачать", color="success", id="btn-download-dataset", className="me-1",style={'width': 'fit-content', 'margin': '5px 12px'}),
+            dbc.Button("Скачать", color="success", id="btn-download-dataset",
+                       className="me-1", style={'width': 'fit-content', 'margin': '5px 12px'}),
         ]),
-        dbc.Row([
-            dash_table.DataTable(df[:300].to_dict('records'),
-                                [{"name": i, "id": i} for i in df.columns],
-                                page_size=30,
-                                style_table={
-                                    'overflowX': 'scroll'
-                                }
-                                )
-        ])
+        dbc.Tabs([
+            dbc.Tab(tab1_content, label="Orders"),
+            dbc.Tab(tab2_content, label="Returns"),
+        ]),
     ])
 ])
-
